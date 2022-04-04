8d08281de82617630318d68ee84940457bf6177c
EC2 Default User <ec2-user@ip-172-31-31-185.us-east-2.compute.internal>
2022-04-04 16:12:08 +0000

    About started

--- src/components/About.js
+++ src/components/About.js
@@ -1,13 +1,18 @@
 import React from 'react';
 
+import jasonPic from '../images/jasonimg2.jpg';
+
 const About = () => {
   return (
     <div className="about">
-      <h1>PORTFOLIO PROFESSIONAL</h1>
-      <h1>PORTFOLIO PROFESSIONAL</h1>
-      <h1>PORTFOLIO PROFESSIONAL</h1>
-      <h1>PORTFOLIO PROFESSIONAL</h1>
-      <h1>PORTFOLIO PROFESSIONAL</h1>
+      <div className="about-main">
+        <div className="about-main-image">
+          <img className="about-image" src={jasonPic} alt="software developer Jason Whisnant"/>
+        </div>
+        <div className="about-main-info">
+          <h1>About Me</h1>
+        </div>
+      </div>
     </div>
   );
 }
