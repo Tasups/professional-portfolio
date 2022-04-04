49e7c5b1469b04f3fc1aaad0f1d1a68d4177c92e
Jason Whisnant <tasups@gmail.com>
2022-04-04 16:39:42 +0000

    Header finished

--- src/components/About.js
+++ src/components/About.js
@@ -11,6 +11,20 @@ const About = () => {
         </div>
         <div className="about-main-info">
           <h1>About Me</h1>
+          <ul>
+            <li>
+              I am a Software Developer with a working proficiency in HTML, CSS, JavaScript, and React. 
+            </li>
+            <li>
+              I love using npm packages such as Axios, uuid, jquery, and redux as well as a few frameworks such as Bootstrap and Material UI and many APIs.
+            </li>
+            <li>
+              I am quite comfortable using Express, Node, Git version control, Cloud9-AWS, AJAX, and MySQL.
+            </li>
+            <li>
+              I possess a fundamental understanding of mobile first design concepts and object oriented programming.
+            </li>
+          </ul>
         </div>
       </div>
     </div>
