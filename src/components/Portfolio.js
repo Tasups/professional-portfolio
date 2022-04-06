46b81da6a19ab9f7f217c6792a9c0fe1dbca4a58
Jason Whisnant <tasups@gmail.com>
2022-04-06 18:36:32 +0000

    hover on projects impossible

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -7,34 +7,45 @@ import snakeGameImg from '../images/SnakeGame.jpg';
 const Portfolio = () => {
   return(
     <div className="portfolio-wrapper container" id="portfolio">
+      <h1>PORTFOLIO</h1>
       <div className="portfolio-list">
-        <div className="portfolio-project">
+      
+        <div className="portfolio-project pp1">
+          <h2 className="portfolio-project-title">BORED? APP</h2>
           <a 
             href="https://eager-kalam-6403d4.netlify.app/" 
             target="_blank" 
             rel="noreferrer"
           >
-            <img src={boredAppImg} alt="bored app" />
+            <img className="pp1-img" src={boredAppImg} alt="bored app" />
+          {/*Using an API, with controlled requests, filtered by preset inputs*/}
           </a>
         </div>
-        <div className="portfolio-project">
+        
+        <div className="portfolio-project pp2">
+          <h2 className="portfolio-project-title">DAILY TASK APP</h2>
           <a 
             href="https://ubiquitous-torrone-aa0502.netlify.app/" 
             target="_blank" 
             rel="noreferrer"
           >
-            <img src={dailyTaskAppImg} alt="daily task app" />
+            <img className="pp2-img" src={dailyTaskAppImg} alt="daily task app" />
+          {/*Internal data sets React's state in this simple CRUD application*/}
           </a>
         </div>
-        <div className="portfolio-project">
+        
+        <div className="portfolio-project pp3">
+          <h2 className="portfolio-project-title">SNAKE GAME</h2>
           <a 
             href="https://precious-cactus-d40834.netlify.app/" 
             target="_blank" 
             rel="noreferrer"
           >
-            <img src={snakeGameImg} alt="snake game app" />
+            <img className="pp3-img" src={snakeGameImg} alt="snake game app" />
+          {/*A simple game mostly using method arrays and useRef hook*/}
           </a>
         </div>
+        
       </div>
     </div>
     )
