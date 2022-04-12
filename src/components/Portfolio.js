a40ba7caa52748301071e1fbd2bbfe012f46a909
Jason Whisnant <tasups@gmail.com>
2022-04-12 14:41:04 -0400

    mostly done? consider overflow or component sizes

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -5,50 +5,48 @@ import dailyTaskAppImg from '../images/DailyTaskApp.jpg';
 import snakeGameImg from '../images/SnakeGame.jpg';
 
 const Portfolio = () => {
-  return(
+  return (
     <div className="portfolio-wrapper" id="portfolio">
+      <h1 className="portfolio-title">PORTFOLIO</h1>
       <div className="portfolio-list">
-         <h1 className="portfolio-title">PORTFOLIO</h1>
         <div className="portfolio-project">
           <h2 className="portfolio-project-title">BORED? APP</h2>
-          <a 
-            href="https://eager-kalam-6403d4.netlify.app/" 
-            target="_blank" 
+          <a
+            href="https://eager-kalam-6403d4.netlify.app/"
+            target="_blank"
             rel="noreferrer"
           >
             <img src={boredAppImg} alt="bored app" />
-          {/*Using an API, with controlled requests, filtered by preset inputs*/}
+            {/*Using an API, with controlled requests, filtered by preset inputs*/}
           </a>
         </div>
-        
+
         <div className="portfolio-project">
           <h2 className="portfolio-project-title">DAILY TASK APP</h2>
-          <a 
-            href="https://ubiquitous-torrone-aa0502.netlify.app/" 
-            target="_blank" 
+          <a
+            href="https://ubiquitous-torrone-aa0502.netlify.app/"
+            target="_blank"
             rel="noreferrer"
           >
             <img src={dailyTaskAppImg} alt="daily task app" />
-          {/*Internal data sets React's state in this simple CRUD application*/}
+            {/*Internal data sets React's state in this simple CRUD application*/}
           </a>
         </div>
-        
+
         <div className="portfolio-project">
           <h2 className="portfolio-project-title">SNAKE GAME</h2>
-          <a 
-            href="https://precious-cactus-d40834.netlify.app/" 
-            target="_blank" 
+          <a
+            href="https://precious-cactus-d40834.netlify.app/"
+            target="_blank"
             rel="noreferrer"
           >
             <img src={snakeGameImg} alt="snake game app" />
-          {/*A simple game mostly using method arrays and useRef hook*/}
+            {/*A simple game mostly using method arrays and useRef hook*/}
           </a>
         </div>
-        
       </div>
-      
     </div>
-    )
+  );
 }
 
 export default Portfolio;
